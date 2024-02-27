ed9a2ab429f64440748194ee9f17508e765c8d2d
balkanarchist <balkanarchist@yandex.com>
2024-02-27 08:05:06 -0700

    added code to dynamic typing exercise

---
+++ dynamic-typing-code-correction.py
@@ -0,0 +1,11 @@
+dynamic_variable = "This is a string"
+# (Your code to print the type of dynamic_variable here)
+print(type(dynamic_variable))
+
+dynamic_variable = 100
+# (Your code to print the type of dynamic_variable here)
+print(type(dynamic_variable))
+
+dynamic_variable = 25.5
+# (Your code to print the type of dynamic_variable here)
+print(type(dynamic_variable))
