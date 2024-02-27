9b9963130da1accd991094a4575fbfb06ac7be75
balkanarchist <balkanarchist@yandex.com>
2024-02-27 09:00:37 -0700

    added code to data-type-code-correction.py

---
+++ data-type-code-correction.py
@@ -0,0 +1,10 @@
+#identify the data type of each variable
+#then use the type() function to print it out
+variable_a = "Hello, World!" #should return as a string
+variable_b = 23 #should return as an int
+variable_c = 3.14 #should return as a float
+variable_d = True #should return as a bool
+print(type(variable_a))
+print(type(variable_b))
+print(type(variable_c))
+print(type(variable_d))
