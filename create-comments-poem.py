b5bd12f8e80990d163e1ea1ba13652ab39734d27
balkanarchist <balkanarchist@yandex.com>
2024-02-26 22:22:29 -0700

    re-added text to poems

---
+++ create-comments-poem.py
@@ -0,0 +1,5 @@
+#Python, in the realm of code you shine,
+#With simplicity and grace, you're truly divine.
+#From simple tasks that ask to greet
+#To if statements that have more meat
+#For Monty's sake, I'll end this line.
---
+++ multi-line-poem.py
@@ -0,0 +1,7 @@
+"""
+Python, in the realm of code you shine,
+With simplicity and grace, you're truly divine.
+I can barely code, much less rhyme.
+In Python'd text I take my time
+And give you all what once was mine.
+"""
