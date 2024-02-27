b2bca0886509778f626f5a1b64de03466625d0e6
balkanarchist <balkanarchist@yandex.com>
2024-02-27 08:37:33 -0700

    added code to your-mood exercise

---
+++ your-mood-today.py
@@ -0,0 +1,11 @@
+'''
+Ask the user how they feel today. 
+If they say "happy", print "That's great to hear!",
+and if they say "sad", print "I hope your day gets better!".
+Ensure your if statement is properly indented.
+'''
+feeling = input('How do you feel today? Please choose between either "happy" or "sad". ')
+if feeling == "happy":
+    print("That's great to hear!")
+if feeling == "sad":
+    print("I hope your day gets better!")
