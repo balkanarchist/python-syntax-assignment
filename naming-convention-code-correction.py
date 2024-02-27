01ab4e2bf0185514f8e75e02b94d5403fad66d39
balkanarchist <balkanarchist@yandex.com>
2024-02-27 09:42:57 -0700

    modified code to adhere to naming conventions

--- naming-convention-code-correction.py
+++ naming-convention-code-correction.py
@@ -6,7 +6,8 @@ to follow convention'''
 #userAge = 25
 #user_location = "New York"
 #MAXLIMIT = 1000
-pi_value = 3.14
-user_age = 25
-user_location = "New York"
-MAXLIMIT = 1000
+
+pi_value = 3.14 #changed to proper snake case form
+user_age = 25 #changed to snake case
+user_location = "New York" #no change
+MAX_LIMIT = 1000 #underscore added to separate words
