46d18b8d22e6d3d0f7b138a9ee38da312fca230e
balkanarchist <balkanarchist@yandex.com>
2024-02-27 19:12:33 -0700

    Added code to grocery-store-math.py

---
+++ grocery-store-math.py
@@ -0,0 +1,9 @@
+# Calculate the total cost of three items
+# you'd commonly find in a grocery store,
+# given their individual prices.
+
+bread_loaf = 1.25
+sushi = 8.99
+ice_cream = 2
+total = str(bread_loaf + sushi + ice_cream) #converted to str to allow for concatenation
+print("The total cost is $" + total + ".")
