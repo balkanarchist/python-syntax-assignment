93cb9e664f0bc719d450fb538ede2ceec8fc57fb
balkanarchist <balkanarchist@yandex.com>
2024-02-27 20:59:03 -0700

    Added code to validating-calculations.py

---
+++ validating-calculations.py
@@ -0,0 +1,9 @@
+# Calculate the value of a particular
+# arithmetic expression twice:
+# once normally, and once using parentheses.
+# Compare the two results to see if they match.
+
+value_a = 4 * 9 - 5
+print(value_a) #should output 31
+value_b = 4 * (9 - 5)
+print(value_b) #should output 16
