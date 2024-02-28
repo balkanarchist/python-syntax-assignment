d63ba3f59614758008227b8c8941aab3fb241cae
balkanarchist <balkanarchist@yandex.com>
2024-02-27 21:36:32 -0700

    Added code to mix-match.py

---
+++ mix-match.py
@@ -0,0 +1,15 @@
+# Combine arithmetic, logical, and comparison
+# operators in a single expression
+# and predict the outcome. Then, compute
+# the expression to see
+# if the prediction was correct.
+
+a = 1
+b = 2
+c = 8
+
+if (a + b) == c or (c - a) >= b:
+    print(True)
+else:
+    print(False)
+# output should be true with the above values
