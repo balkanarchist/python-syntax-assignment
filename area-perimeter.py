f7517359aca43b03a01bca632840de8a22f8cfcb
balkanarchist <balkanarchist@yandex.com>
2024-02-27 20:42:44 -0700

    Added code to area-perimeter.py

---
+++ area-perimeter.py
@@ -0,0 +1,10 @@
+#Given the length and width of a rectangle,
+#compute both its area and perimeter
+#using arithmetic operators
+
+length = 3
+width = 4
+
+area = (length * width)
+perimeter = 2 * (int(length + width))
+print(area, perimeter)
